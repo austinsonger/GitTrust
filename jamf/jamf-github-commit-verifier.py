384b8239a73e4056cc95af7d555a4254f50ccce1
Austin Songer <a.songer@protonmail.com>
2023-12-16 12:02:09 -0600

    Create jamf-github-commit-verifier.py

---
+++ jamf/jamf-github-commit-verifier.py
@@ -0,0 +1,62 @@
+############
+# PURPOSE: This script will interact with GitHub APIs to fetch commit data and then validate it against criteria such as S/MIME signatures, device trust status from JAMF, or other verification methods you have in place.
+############
+
+import json
+import os
+import requests
+
+# Constants
+GITHUB_TOKEN = os.environ['GITHUB_TOKEN']  # GitHub access token
+JAMF_AUTH_TOKEN = os.environ['JAMF_AUTH_TOKEN']  # JAMF API token
+JAMF_URL = "https://your-jamf-instance-url"
+
+def get_commit_data(repo_name, commit_sha):
+    """
+    Fetch commit data from GitHub.
+    """
+    url = f"https://api.github.com/repos/{repo_name}/commits/{commit_sha}"
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def verify_commit_with_jamf(commit_data):
+    """
+    Verify the commit with JAMF for device trust and certificate status.
+    """
+    # Placeholder for JAMF verification logic
+    # This could involve checking if the commit's author's device is managed by JAMF
+    # and if the device is compliant with security policies
+    return True
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda function handler for GitHub commit verification.
+    """
+    # Parse GitHub webhook payload
+    payload = json.loads(event['body'])
+    repo_name = payload['repository']['full_name']
+    commit_sha = payload['head_commit']['id']
+
+    # Get commit data from GitHub
+    commit_data = get_commit_data(repo_name, commit_sha)
+
+    # Verify commit with JAMF
+    verification_result = verify_commit_with_jamf(commit_data)
+
+    # Return the verification result
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'verification_result': verification_result})
+    }
+
+if __name__ == "__main__":
+    # Test the Lambda function locally with dummy data
+    test_event = {
+        'body': json.dumps({
+            'repository': {'full_name': 'your-repo/name'},
+            'head_commit': {'id': 'commit-sha'}
+        })
+    }
+    print(lambda_handler(test_event, None))
