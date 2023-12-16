96dc434ce08b02ecea32637911e623f2c85ed973
Austin Songer <a.songer@protonmail.com>
2023-12-16 12:10:15 -0600

    Update jamf-github-commit-verifier.py

--- jamf/jamf-github-commit-verifier.py
+++ jamf/jamf-github-commit-verifier.py
@@ -25,11 +25,76 @@ def get_commit_data(repo_name, commit_sha):
 def verify_commit_with_jamf(commit_data):
     """
     Verify the commit with JAMF for device trust and certificate status.
+    
+    Args:
+        commit_data (dict): Data about the commit, including author information.
+
+    Returns:
+        bool: True if the commit is verified, False otherwise.
     """
-    # Placeholder for JAMF verification logic
-    # This could involve checking if the commit's author's device is managed by JAMF
-    # and if the device is compliant with security policies
-    return True
+    # Extract author's details from commit_data
+    author_email = commit_data['commit']['author']['email']
+    ################### TO-DO ###################
+    # Placeholder for JAMF API request to get device details
+    # Replace 'device_id' with the appropriate identifier from JAMF
+    device_id = get_device_id_from_email(author_email)
+    device_info = get_device_info_from_jamf(device_id)
+    ################### TO-DO ###################
+    # Placeholder for compliance check logic
+    # This could involve checking if the device is managed by JAMF
+    # and if it complies with security policies
+    is_compliant = check_device_compliance(device_info)
+
+    return is_compliant
+
+def get_device_id_from_email(email):
+    """
+    Get the device ID associated with the given email from JAMF.
+    
+    Args:
+        email (str): The email address of the commit author.
+
+    Returns:
+        str: The device ID associated with the email.
+    """
+    ################### TO-DO ###################
+    # Placeholder for JAMF API request to map email to device ID
+    # Need to replace this with actual API interaction
+    return "device-id-for-author"
+
+def get_device_info_from_jamf(device_id):
+    """
+    Retrieve device information from JAMF using the device ID.
+    
+    Args:
+        device_id (str): The device ID.
+
+    Returns:
+        dict: Information about the device.
+    """
+    ################### TO-DO ###################
+    # Placeholder for JAMF API request to get device details
+    # Replace with actual API interaction
+    # Example API request:
+    # response = requests.get(f"{JAMF_URL}/api/v1/devices/{device_id}", headers={"Authorization": f"Bearer {JAMF_AUTH_TOKEN}"})
+    # response.raise_for_status()
+    # return response.json()
+    return {"managed": True, "compliant": True}
+
+def check_device_compliance(device_info):
+    """
+    Check if the device meets the organization's security policies.
+    
+    Args:
+        device_info (dict): Information about the device.
+
+    Returns:
+        bool: True if the device is compliant, False otherwise.
+    """
+    ################### TO-DO ###################
+    # Placeholder for compliance check logic
+    # Replace with actual compliance check based on device_info
+    return device_info.get('managed', False) and device_info.get('compliant', False)
 
 def lambda_handler(event, context):
     """
