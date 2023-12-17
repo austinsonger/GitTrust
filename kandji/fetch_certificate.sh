6c867c760413268cb5ebf4b9074e4684e1005a53
Austin Songer <a.songer@protonmail.com>
2023-12-16 20:05:45 -0600

    Delete kandji/fetch_certificate.sh

--- kandji/fetch_certificate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Fetch the latest S/MIME certificate ID from Keychain
-# Note: Update the search criteria as per your organization's naming convention
-certificate_id=$(security find-certificate -c "Your-Certificate-Common-Name" -p | openssl x509 -inform pem -noout -text | grep 'Serial Number' | awk '{print $NF}')
-
-echo $certificate_id
