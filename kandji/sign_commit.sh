a7dd48495819495fb1432b03ff737a08c2c19c3f
Austin Songer <a.songer@protonmail.com>
2023-12-16 20:06:36 -0600

    Update sign_commit.sh

--- kandji/sign_commit.sh
+++ kandji/sign_commit.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-# Fetch the latest certificate ID
-certificate_id=$(./fetch_certificate.sh)
+# # Configure Git to use the fetched certificate ID
+# git config --local user.signingkey $certificate_id
 
-# Check if certificate_id is retrieved
-if [ -z "$certificate_id" ]; then
-    echo "Error: Unable to fetch certificate ID."
-    exit 1
-fi
+# Global constants
+email_address="user@example.com"  # Replace with the actual email address
+ca_name="YOUR_CA_NAME"  # Replace with your CA's name
 
-# Configure Git to use the fetched certificate ID
-git config --local user.signingkey $certificate_id
+# Function to fetch S/MIME Key ID using security command (macOS specific)
+fetch_smime_key_id_macos() {
+    local ca_name="$1"
+    local key_id
 
-# Perform the Git commit with sign
-git commit -S -m "$1"
+    # Fetch the current S/MIME certificate key ID from a specific CA
+    key_id=$(security find-identity -p smime -v | grep "$ca_name" | awk '{print $2}')
+
+    if [ -z "$key_id" ]; then
+        echo "No valid S/MIME certificate found for CA: $ca_name"
+        return 1
+    else
+        echo "Key ID for CA $ca_name: $key_id"
+        return 0
+    fi
+}
+
+# Main logic to fetch key ID and set it for git signing
+main() {
+    # Try to fetch key ID using macOS security command
+    local key_fetch_output=$(fetch_smime_key_id_macos "$ca_name")
+
+    if [ $? -eq 0 ]; then
+        # Use smimesign to sign the commit with the fetched key ID
+        smimesign --status-fd=2 -bsau "$key_fetch_output"
+    fi
+}
+
+main
